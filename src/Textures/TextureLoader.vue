3b9e321f1992622cc92f801644860c53e5eb9c6f
Jan Vorisek <jan@vorisek.me>
2023-01-15 00:36:16 +0100

    feat: add wrapS and wrapT to TextureLoader

--- src/Textures/TextureLoader.vue
+++ src/Textures/TextureLoader.vue
@@ -6,34 +6,59 @@ export default {
 </script>
 
 <script setup lang="ts">
-import { inject, ref, watch } from "vue";
+import { inject, ref } from "vue";
 
-import { Texture, TextureLoader, Material } from "three";
+import { Texture, TextureLoader, Material, type Wrapping, ClampToEdgeWrapping } from "three";
+import { handlePropCallback } from "../utils";
 
 export interface Props {
   /**
    *  The path or URL to the texture file. This can also be a Data URI.
    */
   url: string;
+
+  /**
+   * This defines how the texture is wrapped horizontally and corresponds to U in UV mapping.
+   * The default is `THREE.ClampToEdgeWrapping`, where the edge is clamped to the outer edge texels.
+   * The other two choices are `THREE.RepeatWrapping` and `THREE.MirroredRepeatWrapping`.
+   * See the [texture constants](https://threejs.org/docs/?q=texture#api/en/constants/Textures) page for details.
+   */
+  wrapS?: Wrapping;
+
+  /**
+   * This defines how the texture is wrapped vertically and corresponds to V in UV mapping.
+   * The default is `THREE.ClampToEdgeWrapping`, where the edge is clamped to the outer edge texels.
+   * The other two choices are `THREE.RepeatWrapping` and `THREE.MirroredRepeatWrapping`.
+   * See the [texture constants](https://threejs.org/docs/?q=texture#api/en/constants/Textures) page for details.
+   */
+  wrapT?: Wrapping;
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  //
+  wrapS: ClampToEdgeWrapping,
+  wrapT: ClampToEdgeWrapping,
 });
 
 const material = inject("material") as Material;
 
 const three = ref<Texture>(new Texture());
 
-function applyProps(props: Props) {
-  if ("map" in material) material.map = new TextureLoader().load(props.url);
+function load() {
+  three.value = new TextureLoader().load(props.url);
+  if ("map" in material) material.map = three.value;
 }
 
-applyProps(props);
-watch(
-  () => props.url,
-  () => applyProps(props),
-);
+function applyProps() {
+  three.value.wrapS = props.wrapS;
+  three.value.wrapT = props.wrapT;
+}
+
+load();
+
+handlePropCallback(props, "url", load);
+
+handlePropCallback(props, "wrapS", applyProps);
+handlePropCallback(props, "wrapT", applyProps);
 
 defineExpose({ three });
 </script>
