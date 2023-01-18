489a8c735334ec44fba6d449eca7612ca804fc12
Jan Vorisek <jan@vorisek.me>
2023-01-18 11:46:54 +0100

    docs: Release

--- docs/.vitepress/dist/assets/geometries.md.bf7c8114.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as a,f as n}from"./app.7a29b974.js";const d=JSON.parse('{"title":"Geometries","description":"","frontmatter":{},"headers":[],"relativePath":"geometries.md"}'),l={name:"geometries.md"},o=n("",8),t=[o];function p(r,c,i,y,F,m){return s(),a("div",null,t)}const h=e(l,[["render",p]]);export{d as __pageData,h as default};
