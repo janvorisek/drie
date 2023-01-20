99a7180476718006a6988d09c77a47b129b36965
Jan Vorisek <jan@vorisek.me>
2023-01-20 18:26:50 +0100

    feat: mesh can reuse geometry and material

--- src/Objects/Mesh.vue
+++ src/Objects/Mesh.vue
@@ -17,9 +17,9 @@
 </docs>
 
 <script setup lang="ts">
-import { provide, inject, onUnmounted } from "vue";
+import { provide, inject, onUnmounted, onMounted } from "vue";
 
-import { BufferGeometry, Mesh, MeshBasicMaterial, Scene } from "three";
+import { BufferGeometry, Material, Mesh, MeshBasicMaterial, Scene } from "three";
 import { Vector3Like } from "../types";
 import { handleVectorProp, disposeTHREEObject, handlePropCallback, manageParentRelationship } from "../utils";
 
@@ -28,6 +28,10 @@ export interface Props {
    * Whether the object gets rendered into shadow map.
    */
   castShadow?: boolean;
+
+  geometry?: string | null;
+
+  material?: string | null;
 
   /**
    * A [Vector3Like](/types#vector3like) representing the object's local position.
@@ -56,6 +60,8 @@ const props = withDefaults(defineProps<Props>(), {
   scale: () => [1, 1, 1],
   castShadow: false,
   receiveShadow: false,
+  geometry: null,
+  material: null,
 });
 
 const scene = inject("scene") as Scene;
@@ -82,6 +88,22 @@ applyProps();
 
 handlePropCallback(props, "castShadow", applyProps);
 handlePropCallback(props, "receiveShadow", applyProps);
+
+const getGeometry = inject("getGeometry") as (g: string) => BufferGeometry;
+const getMaterial = inject("getMaterial") as (g: string) => Material;
+
+onMounted(() => {
+  if (props.geometry) {
+    const geo = getGeometry(props.geometry);
+    three.geometry = geo;
+  }
+
+  if (props.material) {
+    const geo = getMaterial(props.material);
+    // @ts-expect-error Force material from getMaterial to Mesh
+    three.material = geo;
+  }
+});
 
 onUnmounted(() => {
   scene.remove(three);
