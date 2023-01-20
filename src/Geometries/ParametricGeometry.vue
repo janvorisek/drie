9a243cd7df47f17a0d863005f89d8d77ce59d6b8
Jan Vorisek <jan@vorisek.me>
2023-01-20 13:15:33 +0100

    docs: add ParametricGeometry example code

--- src/Geometries/ParametricGeometry.vue
+++ src/Geometries/ParametricGeometry.vue
@@ -19,6 +19,49 @@ export default {
   <ClientOnly>
     <ParametricGeometry />
   </ClientOnly>
+
+  ::: details Click me to view the example code
+  ```vue{10,25-32}
+  <template>
+    <div style="width: 100vh; height: 100vh;">
+      <Renderer :antialias="true">
+        <PerspectiveCamera :position="[10, 10, 10]" :up="[0, 0, 1]">
+          <OrbitControls />
+        </PerspectiveCamera>
+        <Scene background="#f9f9f9">
+          <Mesh>
+            <MeshNormalMaterial :side="DoubleSide" />
+            <ParametricGeometry
+              name="geo"
+              :function="func"
+              :slices="16"
+              :stacks="16"
+            />
+          </Mesh>
+        </Scene>
+      </Renderer>
+    </div>
+  </template>
+
+  <script setup lang="ts">
+    import { DoubleSide, Vector3 } from "three";
+
+    import { Renderer, Scene } from "@janvorisek/drie";
+    import { PerspectiveCamera, OrbitControls } from "@janvorisek/drie"
+    import { Mesh, ParametricGeometry, MeshNormalMaterial } from "@janvorisek/drie";
+
+    // Parametric equation may be implemented in a following way
+    const func = (u: number, v: number, target: Vector3) => {
+      u = u - 0.5;
+      v = v - 0.5;
+      const x = u * u + v * v;
+      const y = u;
+      const z = v;
+      target.set(10 * x, 10 * y, 10 * z);
+    }
+  </script>
+  ```
+  :::
 </docs>
 
 <script setup lang="ts">
