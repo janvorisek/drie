f304cb26346261d1292fa081fbcf46ae6bc430ea
Jan Vorisek <jan@vorisek.me>
2023-01-24 23:13:03 +0100

    feat: add TextureLoader props - offset, repeat, rotation

--- src/Textures/TextureLoader.vue
+++ src/Textures/TextureLoader.vue
@@ -14,8 +14,28 @@ import { inject, ref } from "vue";
 
 import { Texture, TextureLoader, Material, type Wrapping, ClampToEdgeWrapping } from "three";
 import { handlePropCallback } from "../utils";
+import { Vector2Like } from "../types";
 
 export interface Props {
+  /**
+   * How much a single repetition of the texture is offset from the beginning, in each direction U and V.
+   * Typical range is `0.0` to `1.0`.
+   */
+  offset?: Vector2Like;
+
+  /**
+   * How many times the texture is repeated across the surface, in each direction U and V.
+   * If repeat is set greater than 1 in either direction, the corresponding Wrap parameter should also be set to `THREE.RepeatWrapping` or `THREE.MirroredRepeatWrapping` to achieve the desired tiling effect.
+   * Setting different repeat values for textures is restricted in the same way like `offset`.
+   */
+  repeat?: Vector2Like;
+
+  /**
+   * How much the texture is rotated around the center point, in radians.
+   * Positive values are counter-clockwise.
+   */
+  rotation?: number;
+
   /**
    *  The path or URL to the texture file. This can also be a Data URI.
    */
@@ -41,6 +61,9 @@ export interface Props {
 const props = withDefaults(defineProps<Props>(), {
   wrapS: ClampToEdgeWrapping,
   wrapT: ClampToEdgeWrapping,
+  offset: () => [0, 0],
+  repeat: () => [1, 1],
+  rotation: 0,
 });
 
 const material = inject("material") as Material;
