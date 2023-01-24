ea79657700eeab1117af0a8d30500df13309a25d
Jan Vorisek <jan@vorisek.me>
2023-01-24 23:12:34 +0100

    feat: add Vector2Like type

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
-import { Vector3 } from "three";
+import { Vector2, Vector3 } from "three";
 
 export type Vector3Like = Vector3 | [number, number, number] | { x: number; y: number; z: number };
+export type Vector2Like = Vector2 | [number, number] | { x: number; y: number };
