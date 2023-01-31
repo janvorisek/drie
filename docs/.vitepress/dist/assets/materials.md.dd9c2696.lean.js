4c3aa66cfa5b32a309eea07d87fa488d98f78165
Jan Vorisek <jan@vorisek.me>
2023-02-01 00:38:22 +0100

    docs: Release

--- docs/.vitepress/dist/assets/materials.md.dd9c2696.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as a,c as l,f as e}from"./app.97bf5fb5.js";const d=JSON.parse('{"title":"Materials","description":"","frontmatter":{},"headers":[],"relativePath":"materials.md"}'),n={name:"materials.md"},t=e("",5),o=[t];function p(r,i,c,F,D,y){return a(),l("div",null,o)}const g=s(n,[["render",p]]);export{d as __pageData,g as default};
